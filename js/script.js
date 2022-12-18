8d0bc69140b40b0416e7c6334c8c213e097a3f96
Marcelo Inácio <marceloinacio2121@gmail.com>
2022-12-18 19:23:08 -0300

    inicial

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function modelo_verde(){
+    document.getElementById("product-image").src="img/modelo-verde.jpg"
+}
+
+function modelo_azul(){
+   document.getElementById("product-image").src="img/modelo-azul.jpg"
+}
+
+function modelo_estelar(){
+    document.getElementById("product-image").src="img/modelo-estelar.jpg"
+ }
+
+function modelo_vermelho(){
+    document.getElementById("product-image").src="img/modelo-vermelho.jpg"
+ }
